ea6c4dcfeba0a52d329f18a9dbc55f3eb5ea127d
Shea Newton <shnewto@gmail.com>
2019-05-28 13:51:07 -0700

    A place for excercises and notes while reading through 'Category Theory for Progammers'

---
+++ category-theory-for-progammers/the_essence_of_composition/ident_composition.rs
@@ -0,0 +1,40 @@
+// Shea's take on Chapter 1's exercises 1 and 2 in Rust.
+
+// Identity
+fn id<T>(x: T) -> T {
+    x
+}
+
+// Composition
+fn compose<A, B, C, G, F>(f: F, g: G) -> impl Fn(A) -> C
+where
+    F: Fn(A) -> B,
+    G: Fn(B) -> C,
+{
+    move |x| g(f(x))
+}
+
+// Dummy data to demo compisition
+struct Foo;
+struct Bar;
+struct Baz;
+
+fn first(_: Foo) -> Bar {
+    Bar {}
+}
+
+fn second(_: Bar) -> Baz {
+    Baz {}
+}
+
+fn main() {
+    // Print result of identity function
+    println!("{}", id("Hello, world!"));
+    println!("{}", id(123_456));
+
+    // Compose first and second, get Baz from Foo
+    let c = compose(first, second);
+    let _: Baz = c(Foo {});
+}
+
+
